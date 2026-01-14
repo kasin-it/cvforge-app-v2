611c6a52d3bf772473471c190af8f523c2885cee
kasin-it <kacperosinilo@gmail.com>
2026-01-14 22:36:34 +0100

    chore: update pnpm-lock and clean up README; format tsconfig and layout types

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,8 +1,8 @@
 export default function ProtectedLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <main className="min-h-screen flex flex-col items-center">{children}</main>
   );
